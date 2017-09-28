0a523a6d857a427c04a9da8c5f3574db9837a140
m6tu <32124457+m6tu@users.noreply.github.com>
2017-09-28 12:52:03 +0300

    Vaheulesann1

---
+++ WeatherApp/src/test/java/appTests/WeatherAppTest.java
@@ -0,0 +1,14 @@
+package appTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WeatherAppTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
